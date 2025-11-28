e2f65c9f1f7cdf7bc977d545eea9a60c457caf4b
ColvinKopram <154466436+ColvinKopram@users.noreply.github.com>
2025-11-28 19:27:25 +0000

    changing local host to deployed backend

--- frontend/src/components/crimeMap.tsx
+++ frontend/src/components/crimeMap.tsx
@@ -3,7 +3,7 @@
 const CrimeMap = () => {
   return (
     <iframe
-      src="http://localhost:8080/"
+      src="https://camp-service-353447914077.us-east4.run.app"
       style={{ width: "100%", height: "1000px", border: "none" }}
       title="NYC Crime Map"
     />
